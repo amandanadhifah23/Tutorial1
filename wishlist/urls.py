704e04b151b89a9d7903b217a961bb676029341e
Amanda Nadhifah <amandanadhifah8@gmail.com>
2022-10-06 16:39:23 +0700

    Lab 5 Ajay

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,14 +1,5 @@
 from django.urls import path
-from wishlist.views import show_wishlist
-from wishlist.views import show_xml
-from wishlist.views import show_json #sesuaikan dengan nama fungsi yang dibuat
-from wishlist.views import show_json_by_id
-from wishlist.views import show_xml_by_id
-
-from wishlist.views import register #sesuaikan dengan nama fungsi yang dibuat
-from wishlist.views import login_user #sesuaikan dengan nama fungsi yang dibuat
-from wishlist.views import logout_user #sesuaikan dengan nama fungsi yang dibuat
-
+from wishlist.views import *
 app_name = 'wishlist'
 
 urlpatterns = [
@@ -20,4 +11,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('ajax/', show_wishlist_ajax, name='show_wishlist_ajax'),
+    path('ajax/submit/', data_json, name='tbl-form'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -24,7 +24,25 @@ def show_wishlist(request):
 }
     return render(request, "wishlist.html", context)
 
-# Create your views here.
+@login_required(login_url='/wishlist/login/')
+def show_wishlist_ajax(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+    'list_barang': data_barang_wishlist,
+    'nama': 'Amanda Nadhifah',
+    'last_login': request.COOKIES['last_login'],
+}
+    return render(request, "wishlist_ajax.html", context)
+
+@login_required(login_url='/wishlist/login/')
+def data_json(request):
+    if request.method == 'POST':
+        nama_barang = request.POST.get('nama_barang')
+        harga_barang = request.POST.get('harga_barang')
+        deskripsi = request.POST.get('deskripsi')
+        item = BarangWishlist(nama_barang=nama_barang, harga_barang=harga_barang, deskripsi=deskripsi)
+        item.save()
+        return JsonResponse({"instance": "Proyek Dibuat"}, status=200)
 
 def show_xml(request):
     data = BarangWishlist.objects.all()
